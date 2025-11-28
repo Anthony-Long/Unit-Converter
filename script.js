b52ed31b4b67f6d790d74aa2436dbc21ded73084
Anthony Long <anthonyleelong@gmail.com>
2025-11-28 17:44:28 -0500

    add dom test

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// disable mousewheel on a input number field when in focus
-// (to prevent Chromium browsers change the value when scrolling)
-$('form').on('focus', 'input[type=number]', function (e) {
-  $(this).on('wheel.disableScroll', function (e) {
-    e.preventDefault()
-  })
-})
-$('form').on('blur', 'input[type=number]', function (e) {
-  $(this).off('wheel.disableScroll')
-})
+const convertBtn = document.getElementById('convert-btn')
